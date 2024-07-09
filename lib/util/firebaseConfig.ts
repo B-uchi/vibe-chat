38c435ba41914d4af3c3e180217a5e09de2f49c3
Umeano Ibuchukwu <ibumeano@gmail.com>
2024-07-09 21:59:43 +0100

    setup firestore

--- lib/util/firebaseConfig.ts
+++ lib/util/firebaseConfig.ts
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import dotenv from 'dotenv'
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 dotenv.config()
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,3 +22,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
+export const db = getFirestore(app);
